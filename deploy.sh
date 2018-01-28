d3245b3efc25f616018d7c29d44a698a70ce7f43
Super-Baleine <paul.feuvraux@muonium.ch>
2018-01-28 15:20:20 +0100

    added deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+rel_path="/var/www/html"
+
+echo "Back up: admin-panel & crons"
+cp -r $rel_path/core/cron $rel_path/cron.bckp&&
+echo "Downloading new release..."
+rm -rf $rel_path/core.new
+git clone https://github.com/muonium/core $rel_path/core.new&&
+echo "Setting up configuration..."
+rm -rf $rel_path/core.new/config&&
+cp -r $rel_path/template/config $rel_path/core.new/.&&
+echo "Deploying new release now..."
+rm -rf $rel_path/core
+mv $rel_path/core.new $rel_path/core
+echo "restoring crons & admin-panel"
+mv $rel_path/cron.bckp $rel_path/core/cron
