e09baa02d807df384bc00e69e0e7e4f55c89aee0
Dushy <dylangallin@gmail.com>
2018-05-18 12:24:41 +0200

    Removed comment

--- runEveryDay.php
+++ runEveryDay.php
@@ -3,7 +3,7 @@ require_once("run.php");
 
 $task = new cron();
 //$task->deleteInactiveUsers(); // Do not execute it for now
-//$task->remindSubscriptionsDaysLeft(); // Do not execute it for now
+$task->remindSubscriptionsDaysLeft();
 $task->deleteNotCompletedFiles();
 $task->updateUpgrades();
 ?>
